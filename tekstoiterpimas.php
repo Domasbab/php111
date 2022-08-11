8a5c048af6bce193c605feca696170e66d4bb69f
Domasbab <105633782+Domasbab@users.noreply.github.com>
2022-08-11 09:37:17 +0300

    include

---
+++ tekstoiterpimas.php
@@ -0,0 +1,16 @@
+<?php
+
+echo 'Startas';
+echo '<br>';
+
+include 'tekstoiterpimas2.php';
+
+echo '<br>';
+echo 'And';
+
+
+
+?>
+
+
+// include_once 'tekstoiterpimas2.php'; (Sita naudojam jeigu norim bviena karta iterpti sukurta pasirinkta faila!)
