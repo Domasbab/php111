2b5990f04305c46fe278887d555807d147edcbb4
Domasbab <105633782+Domasbab@users.noreply.github.com>
2022-08-16 08:47:07 +0300

    blue-red

---
+++ webmechanika/lemon.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Location: http://localhost/php111/webmechanika/orange.php');
+die;
+
+?>
+
+<!DOCTYPE html>
+<!-- <meta http-equiv="refresh" content="2; URL='http://localhost/php111/webmechanika/orange.php'" />  -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style= "background:#fafa33">
+    
+</body>
+</html>
